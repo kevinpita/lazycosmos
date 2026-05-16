bcdf37c83aa78a927103f934c4a1778051ddafb6
Kevin Pita <gitkevin@pm.me>
2026-05-16 11:10:36 +0200

    chore: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "lazycosmos development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go_1_26
+            pkgs.just
+            pkgs.golangci-lint
+            pkgs.cobra-cli
+          ];
+        };
+      });
+}
